4acea41c6a05cab183048cfadf5fcd8a1009e61e
AlexanderGarcia502 <pablogarica520hotmaill@gmail.com>
2024-10-26 01:38:56 -0600

    1

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://alexandergarcia502.github.io/microeconomia_project/',
+  base: 'https://alexandergarcia502.github.io/microeconomia_project',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
