ea309bb3a35d931574fc60511bacd84670b34f81
AlexanderGarcia502 <pablogarica520hotmaill@gmail.com>
2024-10-26 02:16:15 -0600

    4

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import ScrollToTop from "@/base-components/scroll-to-top/Main";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import Router from "./router";
 
 function App() {
   return (
     <RecoilRoot>
-      <BrowserRouter basename="/microeconomia_project">
+      <HashRouter>
         <Router />
         <ScrollToTop />
-      </BrowserRouter>
+      </HashRouter>
     </RecoilRoot>
   );
 }
